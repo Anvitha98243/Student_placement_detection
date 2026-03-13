c886a1a6ceb6d47eb999de3a6b807c56cd88a547
anvitha boppana <anvithaboppana999@gmail.com>
2026-03-13 14:40:02 +0530

    add production api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
@@ -9,3 +11,8 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
+```
+
+Then create a new file called **`.env`** inside the `frontend` folder and paste:
+```
+REACT_APP_API_URL=https://placeai-backend.onrender.com/api
